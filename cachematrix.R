909a0889fb8ac216dbfe90de91657793ede98478
MariaBGES <mbg-maria@hotmail.com>
2015-08-23 05:02:02 +0200

    Final

--- cachematrix.R
+++ cachematrix.R
@@ -42,6 +42,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Example:
 ## cacheSolve(exmatrix)
 
+
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   minverse <- x$getinverse()
